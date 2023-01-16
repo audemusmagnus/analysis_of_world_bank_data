a0f1bec49d2c3fc4b360b7010a54e950318fe43d
Blktyee <1proatyourservice@gmail.com>
2023-01-16 16:32:14 -0500

    done with scaled sigmoid

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def nth_power(n, fn = lambda x: x**3):
     """
@@ -8,5 +9,9 @@ def nth_power(n, fn = lambda x: x**3):
     """
     return [fn(i) for i in range(n)]
 
+def sigmoid(x, a=1):
+    return 1/(1 + np.exp(-a*x))
+
 print(nth_power(10))
 print(nth_power(20))
+print(sigmoid(0.0))
